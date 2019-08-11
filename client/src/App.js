6f7ccf068b7ff06b57fa55926fdcaa7d196f237b
beacagg <beatrizcaggianob@gmail.com>
2019-08-11 15:09:13 -0300

    final project

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 import spacex from './spacex.png';
 import ApolloClient from 'apollo-boost';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import {ApolloProvider} from 'react-apollo';
 import Launches from './components/Launches'
+import Launch from './components/Launch'
+
 
 const client = new ApolloClient({
   uri:'http://localhost:5000/graphql'
@@ -12,10 +15,15 @@ const client = new ApolloClient({
 function App() {
   return (  
     <ApolloProvider client={client}>
-    <div className="container">
-     <img src={spacex} alt="SpaceX" style={{width: '50%', display: 'block', margin:'auto'}} />
-      <Launches />
-    </div>
+      <Router>
+        <div className="container">
+        <img src={spacex} alt="SpaceX" style={{width: '50%', display: 'block', margin:'auto'}} />
+          
+        <Route exact path="/" component={Launches} />
+        <Route exact path="/launch/:flight_number" component={Launch} />
+        
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react'
 import gql from 'graphql-tag';
 import {Query} from 'react-apollo';
 import LaunchItem from './LaunchItem'
-
+import MissionKey from './MissionKey'
 
 const LAUNCHES_QUERY = gql`
     query LaunchesQuery{
@@ -20,9 +20,13 @@ export class Launches extends Component {
         return (
             <Fragment>
                 <h1 className="display-4 my-3">Launches</h1>
+                <MissionKey />
                 <Query query={LAUNCHES_QUERY}>
                     {({ loading, error, data }) => {
-                            if(loading) return <h4>Loading...</h4>
+                            if(loading) return( 
+                                <div className="spinner-border" role="status">
+                                <span className="sr-only">Loading...</span>
+                                </div> )
                             if(error) console.log(error);
                             
 
